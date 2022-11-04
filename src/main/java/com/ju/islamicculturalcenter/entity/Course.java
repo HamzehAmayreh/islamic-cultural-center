02a68b6225f3ed1fbf7fb5457857ec7b3c5150d4
ACER <ACER@DESKTOP-D6L6I1H.home>
2022-11-04 19:49:07 +0300

    updated the course entity annotations

--- src/main/java/com/ju/islamicculturalcenter/entity/Course.java
+++ src/main/java/com/ju/islamicculturalcenter/entity/Course.java
@@ -1,17 +1,20 @@
 package com.ju.islamicculturalcenter.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@MappedSuperclass
+@Table
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
