331e897341231ddfc10b40cd903a964a794d85de
Ahmad Altayeh <ahmadaltayeh31@gmail.com>
2022-11-05 12:33:28 +0300

    add constructor to student

--- src/main/java/com/ju/islamicculturalcenter/entity/Student.java
+++ src/main/java/com/ju/islamicculturalcenter/entity/Student.java
@@ -1,20 +1,17 @@
 package com.ju.islamicculturalcenter.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.sql.Timestamp;
 
 @Table(name = "student")
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Student extends Person {
 
     @Column(name = "date_of_birth")
@@ -26,5 +23,11 @@ public class Student extends Person {
     @Column(name = "is_verified")
     private Boolean isVerified;
 
-
+    @Builder
+    public Student(Timestamp creation_Date, Long createdById, Timestamp updateDate, Long updatedById, Boolean active, Long id, String firstName, String lastName, String userName, String email, String password, int phoneNumber, String facebookUrl, Position position, String dateOfBirth, Integer courseCount, Boolean isVerified) {
+        super(creation_Date, createdById, updateDate, updatedById, active, id, firstName, lastName, userName, email, password, phoneNumber, facebookUrl, position);
+        this.dateOfBirth = dateOfBirth;
+        this.courseCount = courseCount;
+        this.isVerified = isVerified;
+    }
 }
